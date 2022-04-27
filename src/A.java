6875c1c75e116e52f41fe35ae149a3aade2132de
mehme <mehmet.tokatli999@gmail.com>
2022-04-27 17:49:46 +0300

    work3

--- src/A.java
+++ src/A.java
@@ -2,6 +2,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("deneme1");
+        System.out.println("deneme2");
 
     }
 }
